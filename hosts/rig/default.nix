86ed004e3ea7a24174f4f66ff5236ff0ef9af5dd
Milo Gertjejansen <milo@milogert.com>
2021-06-20 17:03:25 -0400

    Add rig

--- hosts/rig/default.nix
+++ hosts/rig/default.nix
@@ -4,12 +4,23 @@ rec {
   imports = [
     ./configuration.nix
     ./hardware-configuration.nix
+<<<<<<< HEAD
     ../_common/default-headless.nix
+=======
+    ../_common/default.nix
+>>>>>>> 92998cc (Add rig)
     ./networking.nix
     ./services.nix
     #../_common/services.nix
   ];
 
+<<<<<<< HEAD
+=======
+  programs.sway.enable = true;
+
+  # TODO: This should belong in the rest of the user config.
+
+>>>>>>> 92998cc (Add rig)
   # This is required here for nix-darwin.
   programs.zsh = {
     enable = true;
@@ -20,6 +31,11 @@ rec {
     };
   };
 
+<<<<<<< HEAD
+=======
+  programs.steam.enable = true;
+
+>>>>>>> 92998cc (Add rig)
   environment.variables = {
     HOSTNAME = "rig";
   };
