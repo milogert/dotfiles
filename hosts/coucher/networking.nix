62fce6e81f9a664ecffbfb280313c566a2d6793f
Milo Gertjejansen <milo@milogert.com>
2022-10-31 09:28:55 -0400

    coucher: adds

---
+++ hosts/coucher/networking.nix
@@ -0,0 +1,13 @@
+{
+  networking = {
+    computerName = "coucher";
+    hostName = "coucher";
+    localHostName = "coucher";
+    knownNetworkServices = [
+      "USB 10/100/1000 LAN"
+      "Wi-Fi"
+      "Bluetooth PAN"
+      "Thunderbolt Bridge"
+    ];
+  };
+}
