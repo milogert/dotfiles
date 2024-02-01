4f73942d71ad686c833af9e48143b69264b73c57
Milo Gertjejansen <milo@milogert.com>
2024-01-31 21:32:32 -0600

    feat: texlab

--- modules/neovim/config/lua/milogert/config/lang/main.lua
+++ modules/neovim/config/lua/milogert/config/lang/main.lua
@@ -2,21 +2,22 @@ local log = require("milogert.logger")
 
 -- Load language configurations.
 local optionals = {
-  'css',
-  'elixir',
+  "css",
+  "elixir",
   -- 'haskell',
-  'html',
-  'js',
-  'lua',
-  'nil',
-  'nix',
-  'terraform',
+  "html",
+  "js",
+  "latex",
+  "lua",
+  "nil",
+  "nix",
+  "terraform",
 }
 
 for _, mod in ipairs(optionals) do
-  local ok, err = pcall(require, 'milogert.config.lang.' .. mod)
+  local ok, err = pcall(require, "milogert.config.lang." .. mod)
   if not ok then
-    log.info('Failed to language config file: ' .. mod .. '.lua')
+    log.info("Failed to language config file: " .. mod .. ".lua")
     log.error(err)
   end
 end
