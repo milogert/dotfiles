df7ab44cd70633c458be87486a02fcb3dda97c4b
Milo Gertjejansen <milo@milogert.com>
2025-06-22 00:46:34 -0400

    try dhcp

--- hosts/remote-hog/networking.nix
+++ hosts/remote-hog/networking.nix
@@ -16,30 +16,30 @@
    ];
 
     # defaultGateway = "";
-    defaultGateway6 = {
-      address = "fe80::1";
-      interface = "eth0";
-    };
+    # defaultGateway6 = {
+    #   address = "fe80::1";
+    #   interface = "eth0";
+    # };
 
-    dhcpcd.enable = false;
+    dhcpcd.enable = true;
 
     usePredictableInterfaceNames = lib.mkForce false;
 
-    interfaces = {
-      eth0 = {
-        ipv4.addresses = [
-          { address="91.99.106.170"; prefixLength=32; }
-          { address="100.64.149.42"; prefixLength=32; }
-        ];
-        ipv6.addresses = [
-          { address="2a01:4f8:1c1b:6dc9::1"; prefixLength=64; }
-          { address="fe80::9400:4ff:fe65:32ec"; prefixLength=64; }
-        ];
-        ipv4.routes = [ { address = ""; prefixLength = 32; } ];
-        ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
-      };
-
-    };
+    # interfaces = {
+    #   eth0 = {
+    #     ipv4.addresses = [
+    #       { address="91.99.106.170"; prefixLength=32; }
+    #       { address="100.64.149.42"; prefixLength=32; }
+    #     ];
+    #     ipv6.addresses = [
+    #       { address="2a01:4f8:1c1b:6dc9::1"; prefixLength=64; }
+    #       { address="fe80::9400:4ff:fe65:32ec"; prefixLength=64; }
+    #     ];
+    #     ipv4.routes = [ { address = ""; prefixLength = 32; } ];
+    #     ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
+    #   };
+    #
+    # };
 
     hosts = {
       "2a01:4f8:c010:d56::2" = ["github.com"];
